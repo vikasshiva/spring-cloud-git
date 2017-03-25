6be5360b206229067d80e02b268fc57123075f7d
Vikas Shivashankara <vikas.shivashankara@Vikass-MacBook-Pro.local>
2017-03-26 04:51:44 +0530

    changed location for the properties files in config server

--- core/reservation-service/src/main/java/com/example/ReservationServiceApplication.java
+++ core/reservation-service/src/main/java/com/example/ReservationServiceApplication.java
@@ -14,6 +14,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
@@ -24,7 +25,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ReservationServiceApplication {
 
 	public static void main(String[] args) {
